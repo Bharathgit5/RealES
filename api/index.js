7f2b7db3b4ff6bded2cdaa7b423eb51105284172
bharath <bharathjbiet10@gmail.com>
2024-07-20 19:04:23 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('Connected to Mongodbas');
+}).catch((err) =>{
+    console.log(err);
+})
+
 
 const app = express();
 
 app.listen(3000, () =>{
-    console.log('server is running on port 3000! nodemon is working');
+    console.log('server is running  on port 3000!! nodemon is working db connected');
     //when there is a change here we always need to run again(node api/index.js)
     //instead we can use nodemon
 });
