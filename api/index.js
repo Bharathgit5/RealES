8986d6b2890c84496d420e3f266a2101b56b3e63
bharath <bharathjbiet10@gmail.com>
2024-07-20 18:21:56 +0530

    Backend setup

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.listen(3000, () =>{
-    console.log('server is running on port 3000!! nodemon is working');
+    console.log('server is running on port 3000! nodemon is working');
     //when there is a change here we always need to run again(node api/index.js)
     //instead we can use nodemon
 });
