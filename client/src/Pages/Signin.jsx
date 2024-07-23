1809a716435c893c5b4e0e3ce1c550de5141556f
bharath <bharathjbiet10@gmail.com>
2024-07-24 01:02:23 +0530

    redux added

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
-
+import {useDispatch, useSelector} from 'react-redux'
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userslice'
 const Signin = () => {
   const [formData, setFormData] = useState({});
-  const [error,setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+const {loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange =(e) =>{
    setFormData(
     {
@@ -17,7 +18,7 @@ const Signin = () => {
   const handleSubmit =async (e) =>{
     e.preventDefault();
     try{
-    setLoading(true);
+   dispatch(signInStart())
     const res = await fetch('/api/auth/signin',
       {
         method:"POST",
@@ -30,18 +31,17 @@ const Signin = () => {
     const data = await res.json(); //instead of res.json() in replaced with res.text()
     console.log(data);
     if(data.success === false){
-      setError(data.message);
-      setLoading(false);
+    dispatch(signInFailure(data.message))
       return;
     }
-    setLoading(false);
-    setError(null);
+dispatch(signInSuccess(data));
     //no error in signin then navigate to home
+    //  setLoading(false);
+    //setError(null);
     navigate('/');
   }
   catch(error){
-setLoading(false);
-setError(error.message);
+dispatch(signInFailure(error.message));
   }
   };
 
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Signup = () => {
   const [formData, setFormData] = useState({});
   const [error,setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
   const handleChange =(e) =>{
    setFormData(
     {
@@ -34,10 +33,7 @@ const Signup = () => {
       setLoading(false);
       return;
     }
-    setLoading(false);
-    setError(null);
-    //no error in signup then navigate to signin
-    navigate('/signin');
+    setLoading(false);z
   }
   catch(error){
 setLoading(false);
@@ -47,7 +43,7 @@ setError(error.message);
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
-   <h1 className='text-3xl text-center font-semibold my-7'>SIGN UP</h1>
+   <h1 className='text-3xl text-center font-semibold my-7'>SIGNUP</h1>
 
 <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
   <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username'  onChange={handleChange}/>
@@ -58,7 +54,7 @@ setError(error.message);
 <div className='flex gap-2 mt-5'>
   <p>Have an account?</p>
   <Link to={'/signin'}>
-  <span className='text-blue-700'>Sign-in</span>
+  <span className='text-blue-700'>Signin</span>
   </Link>
 </div>
 {error && <p className='text-red-500 mt-5'>{error}</p>}
