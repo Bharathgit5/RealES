926655ad840c8bb7cba652fbe7a3c73a0a78c44e
bharath <bharathjbiet10@gmail.com>
2024-07-24 00:17:33 +0530

    signin api route

--- api/routes/auth_route.js
+++ api/routes/auth_route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { signup } from '../controllers/auth_controller.js';
+import { signin } from '../controllers/auth_controller.js';
 //.js is must after import
 const router = express.Router();
 
 
 router.post('/signup', signup)
+router.post('/signin', signin)
 export default router;
