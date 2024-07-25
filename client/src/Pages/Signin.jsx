c8241577187eaa14e91b9c8a117c6677d2911a47
bharath <bharathjbiet10@gmail.com>
2024-07-25 19:10:01 +0530

    redux added

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {useDispatch, useSelector} from 'react-redux'
-import { signInStart,signInSuccess,signInFailure } from '../redux/user/userslice'
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice'
 const Signin = () => {
   const [formData, setFormData] = useState({});
 const {loading, error} = useSelector((state) => state.user);
