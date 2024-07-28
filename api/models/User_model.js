64e59dd4a27dba255de4951adfe5cb79bd26e474
bharath <bharathjbiet10@gmail.com>
2024-07-29 01:13:43 +0530

    default profile pic test

--- api/models/User_model.js
+++ api/models/User_model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
   type:String,
-  default:"https://www.bing.com/ck/a?!&&p=12df5dcbec177af5JmltdHM9MTcyMTg2NTYwMCZpZ3VpZD0xMzJkYjZjZC0zMzRlLTZhMWEtMDAxZS1hMmMyMzJiOTZiMzAmaW5zaWQ9NTY3NA&ptn=3&ver=2&hsh=3&fclid=132db6cd-334e-6a1a-001e-a2c232b96b30&u=a1L2ltYWdlcy9zZWFyY2g_cT1qYmlldCUyMGxvZ28mRk9STT1JUUZSQkEmaWQ9MEY0NDQzNDRBQTA3QjJDODdFMDc0Njk4NDEyMzBCRjQ2RDc5NzFFOQ&ntb=1"
+  default:"https://th.bing.com/th/id/OIP.vvmpWt0qBu3LeBgZuUfmGAAAAA?rs=1&pid=ImgDetMain"
     },
 },{timestamps:true});
 
