419b5579ac93ad7b856c9089d1106a46efd9562e
bharath <bharathjbiet10@gmail.com>
2024-07-28 17:54:12 +0530

    Listing page ui

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -4,6 +4,7 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import { app } from '../firebase';
 import { updateUserStart, updateUserFailure, updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import {Link} from 'react-router-dom';
 const Profile = () => {
   const fileRef = useRef(null);
   const {currentUser, loading, error} = useSelector((state) => state.user)
@@ -139,6 +140,9 @@ dispatch(deleteUserSuccess(data));
    onChange={handleChange}   id='password' />
   <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase
   hover:opacity-95 disabled:opacity-85'>{loading ? 'Loading ... ': 'Update'}</button>
+  <Link className='bg-green-700 text-white p-3 rounded-lg
+  uppercase text-center hover:opacity-95' to={'/create-listing'}>
+  Create Listing</Link>
  </form>
  <div className='flex justify-between mt-5'>
   <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delect Account</span>
