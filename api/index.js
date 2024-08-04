44289a4eafdba31b6c8d0c4eb537307579a64863
bharath <bharathjbiet10@gmail.com>
2024-08-04 20:29:24 +0530

    resolved at error at index.js

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use("/api/listing", listingRouter)
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
